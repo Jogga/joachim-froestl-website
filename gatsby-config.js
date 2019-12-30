f3f990a41995854a7103f91dc78dbaad4dbe4330
Joachim Fröstl <hello@jogga.co>
2019-12-30 18:01:16 +0100

    Add slug to markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,18 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {}
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
+}
